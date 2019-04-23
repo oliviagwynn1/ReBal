b3fe02546b9fc9cf60968b718e8926930420bcc8
Olivia Gwynn <oliviagwynn@Olivias-MacBook.local>
2019-04-23 12:03:11 -0400

    Added IP address for Raspberry Pi

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 			isRecording: 0,
 			isToggleOn: true
 		 };
-		socket = socketIOClient("http://localhost:8000/");
+		socket = socketIOClient("https://rebal.netlify.com:8000/");
 		this.handleClick = this.handleClick.bind(this);
 	}
 
